f61b3e934521ccc1fd0876b7f595c2c7dc89973f
Nguyễn Trần Nhật Phương <91233439+NhatPhuong02@users.noreply.github.com>
2022-06-26 03:25:51 +0700

    done

--- admin/admin_footer.php
+++ admin/admin_footer.php
@@ -2,7 +2,7 @@
         class="footer d-flex flex-wrap justify-content-between align-items-center px-5 py-3 mt-auto bg-secondary text-light">
         <span class="smaller-font">&copy; 2022 INTERN Group<br /><span class="xsmall-font">QUANG MINH, NHAT PHUONG, LAP KIEN</span></span>
         <ul class="nav justify-content-end list-unstyled d-flex">
-            <li class="ms-3"><a class="text-light" target="_blank" href=#><i
+            <li class="ms-3"><a class="text-light" target="_blank" href="https://github.com/NhatPhuong02/PHP-Project"><i
                         class="bi bi-github"></i></a></li>
         </ul>
     </footer>
