90a08c5e1b421d250d8819f164866125f7252ef2
NhatPhuong02 <2059032@itec.hcmus.edu.vn>
2022-06-26 21:13:19 +0700

    hello

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
-<footer
-        class="footer d-flex flex-wrap justify-content-between align-items-center px-5 py-3 mt-auto bg-secondary text-light">
+<footer style="background-color: #388087 !important;"
+        class="footer d-flex flex-wrap justify-content-between align-items-center px-5 py-3 mt-auto bg-secondary text-light" >
         <span class="smaller-font">&copy; 2022 INTERN Group<br /><span class="xsmall-font">QUANG MINH, NHAT PHUONG, LAP KIEN</span></span>
         <ul class="nav justify-content-end list-unstyled d-flex">
             <li class="ms-3"><a class="text-light" target="_blank" href="https://github.com/NhatPhuong02/PHP-Project">
--- shop/shop_footer.php
+++ shop/shop_footer.php
@@ -1,4 +1,4 @@
-<footer
+<footer style="background-color: #388087 !important;"
     class="footer d-flex flex-wrap justify-content-between align-items-center px-5 py-3 mt-auto bg-secondary text-light">
     <span class="smaller-font">&copy; 2022 INTERN Group<br /><span class="xsmall-font">QUANG MINH, NHAT PHUONG, LAP KIEN</span></span>
     <ul class="nav justify-content-end list-unstyled d-flex">
